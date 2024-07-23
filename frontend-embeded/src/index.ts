dbca6a6d1239ee15b81c1aebd35ad450792960cd
JureRelja <jurereljanovic99@gmail.com>
2024-07-23 15:31:36 +0200

    new config

--- frontend-embeded/src/index.ts
+++ frontend-embeded/src/index.ts
@@ -1,30 +1,16 @@
-// src/index.ts
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Request, Response } from "express";
 
-/*
- * Load up and parse configuration details from
- * the `.env` file to the `process.env`
- * object of Node.js
- */
-dotenv.config();
+const app = express();
+const port = process.env.PORT || 8080;
 
-/*
- * Create an Express application and get the
- * value of the PORT environment variable
- * from the `process.env`
- */
-const app: Express = express();
-const port = process.env.PORT || 3000;
-
-/* Define a route for the root path ("/")
- using the HTTP GET method */
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get("/", (_req: Request, res: Response) => {
+  return res.send("Express Typescript fd eon Vercel");
 });
 
-/* Start the Express app and listen
- for incoming requests on the specified port */
+app.get("/ping", (_req: Request, res: Response) => {
+  return res.send("pong 🏓");
+});
+
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  return console.log(`Server is listening on ${port}`);
 });
