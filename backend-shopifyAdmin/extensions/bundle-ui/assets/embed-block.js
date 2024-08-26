13f3e2c7d7650abde3af89d36c1dbcf49b590dc3
JureRelja <jurereljanovic99@gmail.com>
2024-08-26 16:34:27 +0200

    established communication with API

--- backend-shopifyAdmin/extensions/bundle-ui/assets/embed-block.js
+++ backend-shopifyAdmin/extensions/bundle-ui/assets/embed-block.js
@@ -1,13 +1,11 @@
 //Embeding the widget in the page
 
-window.onload = function () {
-  const widgetContainer = document.getElementById(
-    "neat-bundles-widget-347204u230",
-  );
+const widgetContainer = document.getElementById(
+  "neat-bundles-widget-347204u230",
+);
 
-  const mainContentShopify = document.getElementById("MainContent");
+const mainContentShopify = document.getElementById("MainContent");
 
-  let clone = widgetContainer.content.cloneNode(true);
+let clone = widgetContainer.content.cloneNode(true);
 
-  mainContentShopify.insertBefore(clone, mainContentShopify.firstChild); //Inserting the widget at the top of the page
-};
+mainContentShopify.insertBefore(clone, mainContentShopify.firstChild); //Inserting the widget at the top of the page
