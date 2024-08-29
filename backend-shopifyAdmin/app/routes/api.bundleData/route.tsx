6012b8fad338c3936e1c2368997ab70677288725
JureRelja <jurereljanovic99@gmail.com>
2024-08-29 17:15:43 +0200

    fixed sorting on bundleStep retreval

--- backend-shopifyAdmin/app/routes/api.bundleData/route.tsx
+++ backend-shopifyAdmin/app/routes/api.bundleData/route.tsx
@@ -8,6 +8,7 @@ import {
 import db from "~/db.server";
 import { JsonData } from "~/types/jsonData";
 import { checkPublicAuth } from "~/utils/publicApi.auth";
+import { ReadCache, WriteCache } from "../cache";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
@@ -20,55 +21,19 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
   await checkPublicAuth(storeUrl, bundleId); //Public auth check
 
-  let data: BundleAllResources | BundleBasicAndSettings | null = null;
-  // Returning bundle alone or with selected step
-  if (bundleId && stepNumber) {
-    try {
-      data = (await db.bundle.findUnique({
-        where: {
-          id: Number(bundleId),
-        },
-        include: bundleAllResources,
-      })) as BundleAllResources;
-    } catch (error) {
-      console.log(error);
-    }
-  } else if (bundleId && stepId) {
-    try {
-      data = (await db.bundle.findUnique({
-        where: {
-          id: Number(bundleId),
-        },
-        include: {
-          steps: {
-            where: {
-              id: Number(stepId),
-            },
-          },
-        },
-      })) as BundleAllResources;
-    } catch (error) {
-      console.log(error);
-    }
-  } else {
-    try {
-      data = (await db.bundle.findUnique({
-        where: {
-          id: Number(bundleId),
-        },
-        include: bundleAllResources,
-      })) as BundleAllResources;
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  //Cache aside
+  const key = `api/bundleData/${storeUrl}/${bundleId}`;
+  const cacheData = await ReadCache(key);
 
-  if (!data) {
+  if (cacheData) {
     return json(
       new JsonData(
-        false,
-        "error",
-        "There was an error with your request. 'stepNum' or 'stepId' doesn't belong to this bundle.",
+        true,
+        "success",
+        "Bundle succesfuly retirieved.",
+        [],
+        cacheData,
+        true,
       ),
       {
         headers: {
@@ -77,15 +42,80 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
         status: 200,
       },
     );
-  }
+  } else {
+    let data: BundleAllResources | BundleBasicAndSettings | null = null;
+    // Returning bundle alone or with selected step
+    if (bundleId && stepNumber) {
+      try {
+        data = (await db.bundle.findUnique({
+          where: {
+            id: Number(bundleId),
+          },
+          include: bundleAllResources,
+        })) as BundleAllResources;
+      } catch (error) {
+        console.log(error);
+      }
+    } else if (bundleId && stepId) {
+      try {
+        data = (await db.bundle.findUnique({
+          where: {
+            id: Number(bundleId),
+          },
+          include: {
+            steps: {
+              where: {
+                id: Number(stepId),
+              },
+              orderBy: {
+                stepNumber: "asc",
+              },
+            },
+          },
+        })) as BundleAllResources;
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      try {
+        data = (await db.bundle.findUnique({
+          where: {
+            id: Number(bundleId),
+          },
+          include: bundleAllResources,
+        })) as BundleAllResources;
+      } catch (error) {
+        console.log(error);
+      }
+    }
 
-  return json(
-    new JsonData(false, "success", "Bundle succesfuly retirieved.", [], data),
-    {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
+    if (!data) {
+      return json(
+        new JsonData(
+          false,
+          "error",
+          "There was an error with your request. 'stepNum' or 'stepId' doesn't belong to this bundle.",
+        ),
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+          status: 200,
+        },
+      );
+    }
+
+    //Write to cache
+    await WriteCache(key, data);
+
+    return json(
+      new JsonData(false, "success", "Bundle succesfuly retirieved.", [], data),
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        status: 200,
       },
-      status: 200,
-    },
-  );
+    );
+  }
 };
