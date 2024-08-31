73f3759d3765c4b90ffc6cfe51d0b74913236ead
JureRelja <jurereljanovic99@gmail.com>
2024-08-31 12:37:35 +0200

    moneyFormating now works

--- backend-shopifyAdmin/extensions/bundle-ui/assets/widget.js
+++ backend-shopifyAdmin/extensions/bundle-ui/assets/widget.js
@@ -1,22 +1,42 @@
-const APP_URL = "https://sodium-snake-tommy-contributors.trycloudflare.com";
+//Function to format money
 
-document.addEventListener("alpine:init", () => {
-  Alpine.data("bundle", () => ({
-    init() {
-      console.log(
-        `${APP_URL}/api/bundleData?bundleId={{bundleId}}&storeUrl={{shop.permanent_domain}}`,
-      );
-      fetch(
-        `${APP_URL}/api/bundleData?bundleId={{bundleId}}&storeUrl={{shop.permanent_domain}}`,
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          this.bundleData = data;
-        })
-        .catch((error) => console.log("error", error));
-    },
-
-    shopDomain: "{{ shop.permanent_domain }}",
-    bundleId: "{{ bundleId }}",
-  }));
-});
+//https://gist.github.com/stewartknapman/8d8733ea58d2314c373e94114472d44c
+Shopify.formatMoney = function (cents, format) {
+  if (typeof cents == "string") {
+    cents = cents.replace(".", "");
+  }
+  var value = "";
+  var placeholderRegex = /{{\s*(\w+)\s*}}/;
+  var formatString = format || this.money_format;
+  function defaultOption(opt, def) {
+    return typeof opt == "undefined" ? def : opt;
+  }
+  function formatWithDelimiters(number, precision, thousands, decimal) {
+    precision = defaultOption(precision, 2);
+    thousands = defaultOption(thousands, ",");
+    decimal = defaultOption(decimal, ".");
+    if (isNaN(number) || number == null) {
+      return 0;
+    }
+    number = (number / 100.0).toFixed(precision);
+    var parts = number.split("."),
+      dollars = parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1" + thousands),
+      cents = parts[1] ? decimal + parts[1] : "";
+    return dollars + cents;
+  }
+  switch (formatString.match(placeholderRegex)[1]) {
+    case "amount":
+      value = formatWithDelimiters(cents, 2);
+      break;
+    case "amount_no_decimals":
+      value = formatWithDelimiters(cents, 0);
+      break;
+    case "amount_with_comma_separator":
+      value = formatWithDelimiters(cents, 2, ".", ",");
+      break;
+    case "amount_no_decimals_with_comma_separator":
+      value = formatWithDelimiters(cents, 0, ".", ",");
+      break;
+  }
+  return formatString.replace(placeholderRegex, value);
+};
