a68c224033fdf35de8b9834afe126267cb23164d
JureRelja <jurereljanovic99@gmail.com>
2024-10-30 14:13:56 +0100

    using absolute path

--- app/adminBackend/repository/impl/ShopifyProductVariantRepository.ts
+++ app/adminBackend/repository/impl/ShopifyProductVariantRepository.ts
@@ -1,6 +1,6 @@
 import { AdminApiContext } from "@shopify/shopify-app-remix/server";
-import { AddedProductVariantDto } from "~/adminBackend/service/dto/AddedProductVariantDto";
-import { PriceCalculationType } from "~/adminBackend/shopifyGraphql/graphql";
+import { AddedProductVariantDto } from "app/adminBackend/service/dto/AddedProductVariantDto";
+import { PriceCalculationType } from "app/adminBackend/shopifyGraphql/graphql";
 
 export class ShopifyProductVariantRepository {
     constructor() {}
