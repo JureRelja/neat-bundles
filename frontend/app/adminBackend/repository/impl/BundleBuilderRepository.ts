0b08d66ae177265dc72723821e191bef8ec6abb8
JureRelja <jurereljanovic99@gmail.com>
2025-02-05 16:38:35 +0100

    fix: removed shopify page service

--- frontend/app/adminBackend/repository/impl/BundleBuilderRepository.ts
+++ frontend/app/adminBackend/repository/impl/BundleBuilderRepository.ts
@@ -1,153 +1,20 @@
-import type { Page } from "~/adminBackend/shopifyGraphql/graphql";
 import db from "../../../db.server";
 import type { BundleBuilder } from "@prisma/client";
 import type { BundleBuilderAndBundleSteps } from "@adminBackend/model/BundleBuilderAndBundleSteps";
 import { bundleBuilderAndBundleSteps } from "@adminBackend/model/BundleBuilderAndBundleSteps";
 export class BundleBuilderRepository {
-    public static async createNewBundleBuilder(shop: string, bundleTitle: string, bundleProductId: string, bundlePageId: string, bundleBuilderPageHandle: string) {
-        //Create a new bundle in the database
-        const bundle = await db.bundleBuilder.create({
+    public async create(createBundleBuilderDto: CreateBundleBuilderDto): Promise<BundleBuilderEntity> {
+        const bundleBuilder: BundleBuilderEntity = await db.bundleBuilder.create({
             data: {
-                user: {
+                User: {
                     connect: {
-                        storeUrl: shop,
+                        shop: createBundleBuilderDto.shop,
                     },
                 },
-                title: bundleTitle,
+                title: createBundleBuilderDto.title,
                 published: true,
-                shopifyProductId: bundleProductId,
-                shopifyPageId: bundlePageId,
-                pricing: "CALCULATED",
-                discountType: "PERCENTAGE",
-                discountValue: 10,
-                bundleSettings: {
-                    create: {
-                        skipTheCart: false,
-                        allowBackNavigation: true,
-                        showOutOfStockProducts: false,
-                    },
-                },
-                bundleBuilderPageHandle: bundleBuilderPageHandle,
-                steps: {
-                    create: [
-                        {
-                            stepNumber: 1,
-                            title: "Step 1",
-                            stepType: "PRODUCT",
-                            description: "This is a description for Step 1",
-                            productInput: {
-                                create: {
-                                    minProductsOnStep: 1,
-                                    maxProductsOnStep: 3,
-                                    allowProductDuplicates: false,
-                                    showProductPrice: true,
-                                },
-                            },
-                            contentInputs: {
-                                create: [
-                                    {
-                                        inputType: "TEXT",
-                                        inputLabel: "Enter text",
-                                        maxChars: 50,
-                                        required: true,
-                                    },
-                                    {
-                                        inputLabel: "",
-                                        maxChars: 0,
-                                        required: false,
-                                        inputType: "NONE",
-                                    },
-                                ],
-                            },
-                        },
-                        {
-                            stepNumber: 2,
-                            title: "Step 2",
-                            description: "This is a description for Step 2",
-
-                            stepType: "PRODUCT",
-                            productInput: {
-                                create: {
-                                    minProductsOnStep: 1,
-                                    maxProductsOnStep: 3,
-                                    allowProductDuplicates: false,
-                                    showProductPrice: true,
-                                },
-                            },
-                            contentInputs: {
-                                create: [
-                                    {
-                                        inputType: "TEXT",
-                                        inputLabel: "Enter text",
-                                        maxChars: 50,
-                                        required: true,
-                                    },
-                                    {
-                                        inputLabel: "",
-                                        maxChars: 0,
-                                        required: false,
-                                        inputType: "NONE",
-                                    },
-                                ],
-                            },
-                        },
-                        {
-                            stepNumber: 3,
-                            title: "Step 3",
-                            description: "This is a description for Step 3",
-
-                            stepType: "PRODUCT",
-                            productInput: {
-                                create: {
-                                    minProductsOnStep: 1,
-                                    maxProductsOnStep: 3,
-                                    allowProductDuplicates: false,
-                                    showProductPrice: true,
-                                },
-                            },
-                            contentInputs: {
-                                create: [
-                                    {
-                                        inputType: "TEXT",
-                                        inputLabel: "Enter text",
-                                        maxChars: 50,
-                                        required: true,
-                                    },
-                                    {
-                                        inputLabel: "",
-                                        maxChars: 0,
-                                        required: false,
-                                        inputType: "NONE",
-                                    },
-                                ],
-                            },
-                        },
-                    ],
-                },
-            },
-        });
-
-        return bundle;
-    }
-
-    public static async createNewEmptyBundleBuilder(shop: string, bundleTitle: string, bundleProductId: string, bundlePageId: string, bundleBuilderPageHandle: string) {
-        //Create a new bundle in the database
-        const bundleBuilder = await db.bundleBuilder.create({
-            data: {
-                user: {
-                    connect: {
-                        storeUrl: shop,
-                    },
-                },
-                title: bundleTitle,
-                published: true,
-                shopifyProductId: bundleProductId,
-                shopifyPageId: bundlePageId,
-                bundleBuilderPageHandle: bundleBuilderPageHandle,
-                pricing: "CALCULATED",
-                discountType: "PERCENTAGE",
-                discountValue: 10,
-                bundleSettings: {
+                shopifyProductId: createBundleBuilderDto.title,
+                BundleBuilderConfig: {
                     create: {
                         skipTheCart: false,
                         allowBackNavigation: true,
@@ -160,23 +27,14 @@ export class BundleBuilderRepository {
         return bundleBuilder;
     }
 
-    public async getAllActiveBundleBuilders(storeUrl: string): Promise<BundleBuilder[] | null> {
-        return db.bundleBuilder.findMany({
-            where: {
-                storeUrl: storeUrl,
-                deleted: false,
-            },
-        });
-    }
-
-    public async getFirstActiveBundleBuilder(storeUrl: string): Promise<BundleBuilder | null> {
-        return db.bundleBuilder.findFirst({
-            where: {
-                storeUrl: storeUrl,
-                deleted: false,
-            },
-        });
-    }
+    // public async getFirstActiveBundleBuilder(storeUrl: string): Promise<BundleBuilder | null> {
+    //     return db.bundleBuilder.findFirst({
+    //         where: {
+    //             storeUrl: storeUrl,
+    //             deleted: false,
+    //         },
+    //     });
+    // }
 
     public static async getBundleBuilderById(bundleBuilderId: number) {
         return db.bundleBuilder.findUnique({
@@ -186,72 +44,14 @@ export class BundleBuilderRepository {
         });
     }
 
-    public static async updateBundleBuilderProductId(bundleBuilderId: number, productId: string) {
-        await db.bundleBuilder.update({
-            where: {
-                id: bundleBuilderId,
-            },
-            data: {
-                shopifyProductId: productId,
-            },
-        });
-    }
-
-    public static async updateBundleBuilderPage(bundleBuilderId: number, newPage: Page) {
-        await db.bundleBuilder.update({
-            where: {
-                id: bundleBuilderId,
-            },
-            data: {
-                shopifyPageId: newPage.id,
-                bundleBuilderPageHandle: newPage.handle,
-            },
-        });
-    }
-
-    public static async getMaxBundleBuilderId(shop: string) {
-        const { _max }: { _max: { id: number | null } } = await db.bundleBuilder.aggregate({
-            _max: {
-                id: true,
-            },
-            where: {
-                storeUrl: shop,
-            },
-        });
-
-        return _max.id;
-    }
-
-    public async getBundleBuilderCountByStoreUrl(storeUrl: string): Promise<number> {
-        return db.bundleBuilder.count({
-            where: {
-                storeUrl: storeUrl,
-                deleted: false,
-            },
-        });
-    }
-
-    public async getBundleBuilderByIdAndStoreUrl(id: number, storeUrl: string): Promise<BundleBuilder | null> {
-        return db.bundleBuilder.findUnique({
-            where: {
-                id: id,
-                storeUrl: storeUrl,
-                deleted: false,
-            },
-        });
-    }
-
     public async getAllBundleBuilderAndBundleSteps(storeUrl: string): Promise<BundleBuilderAndBundleSteps[] | null> {
         const bundleBuilders: BundleBuilderAndBundleSteps[] = await db.bundleBuilder.findMany({
             where: {
-                user: {
-                    storeUrl: storeUrl,
-                },
-                deleted: false,
+                shop: storeUrl,
             },
             include: bundleBuilderAndBundleSteps,
             orderBy: {
-                createdAt: "desc",
+                createdAt: "asc",
             },
         });
 
@@ -261,10 +61,7 @@ export class BundleBuilderRepository {
     public async getAllBundleBuilderAndBundleStepsAsc(storeUrl: string): Promise<BundleBuilderAndBundleSteps[] | null> {
         const bundleBuilders: BundleBuilderAndBundleSteps[] = await db.bundleBuilder.findMany({
             where: {
-                user: {
-                    storeUrl: storeUrl,
-                },
-                deleted: false,
+                shop: storeUrl,
             },
             include: bundleBuilderAndBundleSteps,
             orderBy: {
@@ -276,25 +73,72 @@ export class BundleBuilderRepository {
     }
 
     public async deleteBundleBuilderById(id: number): Promise<undefined> {
-        await db.bundleBuilder.update({
+        await db.bundleBuilder.delete({
             where: {
                 id: id,
-            },
-            data: {
-                deleted: true,
             },
         });
     }
 
     public async deleteBundles(bundlesForDeleting: number[]): Promise<undefined> {
-        await db.bundleBuilder.updateMany({
+        await db.bundleBuilder.deleteMany({
             where: {
                 id: {
                     in: bundlesForDeleting,
                 },
             },
-            data: {
-                deleted: true,
+        });
+    }
+
+    public async delete(id: number): Promise<BundleBuilderEntity | null> {
+        return await db.bundleBuilder.update({
+            where: {
+                id: id,
+            },
+            data: {},
+        });
+    }
+
+    public async get(id: number, shop: string): Promise<BundleBuilderEntity | null> {
+        return db.bundleBuilder.findUnique({
+            where: {
+                id: id,
+                shop: shop,
+            },
+        });
+    }
+
+    async getWithSteps(id: number, shop: string): Promise<BundleBuilderAndStepsBasicDto | null> {
+        return db.bundleBuilder.findUnique({
+            where: {
+                id: id,
+                shop: shop,
+            },
+            select: bundleBuilderAndStepsBasicSelect,
+        });
+    }
+
+    public async getAll(shop: string): Promise<BundleBuilderEntity[] | null> {
+        return db.bundleBuilder.findMany({
+            where: {
+                shop: shop,
+            },
+        });
+    }
+
+    public async update(updateBundleBuilderDto: UpdateBundleBuilderDto): Promise<BundleBuilderEntity> {
+        return await db.bundleBuilder.update({
+            where: {
+                id: updateBundleBuilderDto.id,
+            },
+            data: updateBundleBuilderDto,
+        });
+    }
+
+    public async getCount(shop: string): Promise<number> {
+        return db.bundleBuilder.count({
+            where: {
+                shop: shop,
             },
         });
     }
